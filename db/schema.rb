048aa0e9ebc34f5498c2c1bdd90f136353a0f56d
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-04 00:15:09 -0500

    Topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_004308) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_005248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_004308) do
 
   create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.uuid "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,24 @@ roles.each do |role|
   user = Account.create(email: "#{role.name}@example.com", password: "password")
   user.roles << role
 end
+topics = JSON.parse(File.read(File.join(File.dirname(__FILE__), "seeds/topics.json")))
+
+def create_topics(name, children, parent_id = nil)
+  topic = Topic.find_or_create_by(name: name, parent_id: parent_id)
+  puts "Creating topic: #{topic.name} with full path: #{topic.full_path}" # rubocop:disable Rails/Output
+
+  if children.is_a? Array
+    children.each do |child_name|
+      create_topics(child_name, [], topic.id)
+    end
+  elsif children.is_a? Hash
+    children.each do |child_name, grand_children|
+      create_topics(child_name, grand_children, topic.id)
+    end
+  end
+end
+
+# Iterate over the topics and create them
+topics.each do |name, children|
+  create_topics(name, children)
+end
