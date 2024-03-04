32f61b9773cd9740a7479e7cb38a788ef345943c
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-03 19:40:03 -0500

    Setup Devise

--- spec/views/static/tos.html.erb_spec.rb
+++ spec/views/static/tos.html.erb_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "static/tos.html.erb", type: :view do
+RSpec.describe "static/tos.html.erb" do
   pending "add some examples to (or delete) #{__FILE__}"
 end
