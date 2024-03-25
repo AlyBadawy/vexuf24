028219f711b20c44bd3a21192783691013ac10c8
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-24 23:20:46 -0400

    Comply with Rubocop cops -5

--- spec/factories/therapy_sessions.rb
+++ spec/factories/therapy_sessions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :therapy_session do
-    association :therapist, strategy: :build
-    association :patient, strategy: :build
-    session_datetime { "2024-03-04 15:12:22" }
+    association :therapist, factory: :account, strategy: :build
+    association :patient, factory: :account, strategy: :build
+    session_datetime { Time.zone.parse("2024-03-04 15:12:22") }
   end
 end
