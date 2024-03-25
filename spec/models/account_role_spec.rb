1a4b91a5480deab0e09f914f9aa59dd4bedd8642
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-24 23:12:19 -0400

    Comply with Rubocop cops -2

--- spec/models/account_role_spec.rb
+++ spec/models/account_role_spec.rb
@@ -5,11 +5,4 @@ RSpec.describe AccountRole do
     it { is_expected.to belong_to(:account) }
     it { is_expected.to belong_to(:role) }
   end
-
-  describe "validations" do
-    subject(:account_role) { build(:account_role) }
-
-    it { is_expected.to validate_presence_of(:account) }
-    it { is_expected.to validate_presence_of(:role) }
-  end
 end
