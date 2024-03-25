73f4cb9ac64278e2fbd1aff15efe9ff4e849ab69
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-24 23:13:34 -0400

    Comply with Rubocop cops -4

--- spec/factories/therapy_sessions.rb
+++ spec/factories/therapy_sessions.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
-  # rubocop:disable FactoryBot/FactoryAssociationWithStrategy
   factory :therapy_session do
+    association :therapist, strategy: :build
+    association :patient, strategy: :build
     session_datetime { "2024-03-04 15:12:22" }
-    patient { create(:account, :patient) }
-    therapist { create(:account, :therapist) }
   end
-  # rubocop:enable FactoryBot/FactoryAssociationWithStrategy
 end
