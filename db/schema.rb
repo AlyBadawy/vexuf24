84b4aede5374df1e442a218bcab8c06efb183bae
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-03-26 20:45:16 -0400

    Fix Schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_171142) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_060651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -98,9 +98,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_171142) do
 
   create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
+    t.string "description"
+    t.string "icon"
+    t.integer "position", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "position", default: 0, null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
